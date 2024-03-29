dc8f7b25ba6c049f601ba9da7496850866d425b3
Julian Hofmann <git@julianh.de>
2024-03-29 17:57:00 +0100

    Give feedback when passkey actions fail

--- data/static/js/createPasskey.js
+++ data/static/js/createPasskey.js
@@ -21,48 +21,53 @@ const passwordInput = document.getElementById("password");
 const wrongPassword = document.getElementById("wrong-password");
 form.addEventListener("submit", async (e) => {
   e.preventDefault();
-  const res = await fetch("/user/passkey/create/begin", {
-    method: "POST",
-    body: JSON.stringify({
-      name: nameInput.value,
-      password: passwordInput.value
-    })
-  });
-  if (res.status === 401) {
-    passwordInput.classList.add("invalid-field");
-    wrongPassword.classList.remove("invisible");
-    return;
-  } else if (res.status !== 200) {
-    alert("ERROR: status: " + res.status);
-    return;
-  }
-  passwordInput.classList.remove("invalid-field");
-  wrongPassword.classList.add("invisible");
-  const authOptions = await res.json();
-  authOptions.publicKey.user.id = decode(authOptions.publicKey.user.id);
-  authOptions.publicKey.challenge = decode(authOptions.publicKey.challenge);
-  const webAuthnResponse = await navigator.credentials.create({
-    publicKey: {
-      ...authOptions.publicKey,
-      userVerification: "preferred",
+  try {
+    const res = await fetch("/user/passkey/create/begin", {
+      method: "POST",
+      body: JSON.stringify({
+        name: nameInput.value,
+        password: passwordInput.value
+      })
+    });
+    if (res.status === 401) {
+      passwordInput.classList.add("invalid-field");
+      wrongPassword.classList.remove("invisible");
+      return;
+    } else if (res.status !== 200) {
+      alert("ERROR: status: " + res.status);
+      return;
     }
-  });
-  if (!webAuthnResponse) return;
-  const res2 = await fetch("/user/passkey/create/finish", {
-    method: "POST",
-    body: JSON.stringify({
-      id: webAuthnResponse.id,
-      type: webAuthnResponse.type,
-      rawId: encode(webAuthnResponse.rawId),
-      response: {
-        attestationObject: encode(webAuthnResponse.response.attestationObject),
-        clientDataJSON: encode(webAuthnResponse.response.clientDataJSON)
+    passwordInput.classList.remove("invalid-field");
+    wrongPassword.classList.add("invisible");
+    const authOptions = await res.json();
+    authOptions.publicKey.user.id = decode(authOptions.publicKey.user.id);
+    authOptions.publicKey.challenge = decode(authOptions.publicKey.challenge);
+    const webAuthnResponse = await navigator.credentials.create({
+      publicKey: {
+        ...authOptions.publicKey,
+        userVerification: "preferred",
       }
-    })
-  });
-  if (res2.status !== 201) {
-    alert("ERROR: status: " + res2.status);
-    return;
+    });
+    if (!webAuthnResponse) return;
+    const res2 = await fetch("/user/passkey/create/finish", {
+      method: "POST",
+      body: JSON.stringify({
+        id: webAuthnResponse.id,
+        type: webAuthnResponse.type,
+        rawId: encode(webAuthnResponse.rawId),
+        response: {
+          attestationObject: encode(webAuthnResponse.response.attestationObject),
+          clientDataJSON: encode(webAuthnResponse.response.clientDataJSON)
+        }
+      })
+    });
+    if (res2.status !== 201) {
+      alert("ERROR: status: " + res2.status);
+      return;
+    }
+    location.href = "/user/passkey"
+  } catch (e) {
+    console.error(e);
+    alert("Action failed.")
   }
-  location.href = "/user/passkey"
 }, true);
--- data/static/js/login.js
+++ data/static/js/login.js
@@ -20,44 +20,48 @@ const errorList = document.getElementById("login-error-list");
 const invalidCredentialsError = document.getElementById("invalid-credentials");
 passkeyBtn.addEventListener("click", async (e) => {
   e.preventDefault()
-  console.log("passkey")
-  errorList.replaceChildren(invalidCredentialsError)
-  errorList.classList.add("invisible");
-  invalidCredentialsError.classList.add("invisible");
-  console.log("passkey begin")
-  const res = await fetch("/user/passkey/verify/begin", { method: "POST" });
-  if (res.status !== 200) {
-    alert("ERROR: status: " + res.status);
-    return;
-  }
-  const authOptions = await res.json();
-  authOptions.publicKey.challenge = decode(authOptions.publicKey.challenge);
-  const credential = await navigator.credentials.get({
-    publicKey: authOptions.publicKey
-  });
-  console.log("passkey finish")
-  const res2 = await fetch("/user/passkey/verify/finish", {
-    method: "POST",
-    body: JSON.stringify({
-      id: credential.id,
-      type: credential.type,
-      rawId: encode(credential.rawId),
-      response: {
-        authenticatorData: encode(credential.response.authenticatorData),
-        signature: encode(credential.response.signature),
-        userHandle: encode(credential.response.userHandle),
-        clientDataJSON: encode(credential.response.clientDataJSON)
-      }
-    })
-  });
-  if (res2.status !== 200) {
-    if (res2.status === 401) {
-      errorList.classList.remove("invisible");
-      invalidCredentialsError.classList.remove("invisible");
-    } else {
-      alert("ERROR: status: " + res2.status)
+  try {
+    console.log("passkey")
+    errorList.replaceChildren(invalidCredentialsError)
+    errorList.classList.add("invisible");
+    invalidCredentialsError.classList.add("invisible");
+    console.log("passkey begin")
+    const res = await fetch("/user/passkey/verify/begin", { method: "POST" });
+    if (res.status !== 200) {
+      alert("ERROR: status: " + res.status);
+      return;
     }
-    return;
+    const authOptions = await res.json();
+    authOptions.publicKey.challenge = decode(authOptions.publicKey.challenge);
+    const credential = await navigator.credentials.get({
+      publicKey: authOptions.publicKey
+    });
+    console.log("passkey finish")
+    const res2 = await fetch("/user/passkey/verify/finish", {
+      method: "POST",
+      body: JSON.stringify({
+        id: credential.id,
+        type: credential.type,
+        rawId: encode(credential.rawId),
+        response: {
+          authenticatorData: encode(credential.response.authenticatorData),
+          signature: encode(credential.response.signature),
+          userHandle: encode(credential.response.userHandle),
+          clientDataJSON: encode(credential.response.clientDataJSON)
+        }
+      })
+    });
+    if (res2.status !== 200) {
+      if (res2.status === 401) {
+        errorList.classList.remove("invisible");
+        invalidCredentialsError.classList.remove("invisible");
+      } else {
+        alert("ERROR: status: " + res2.status)
+      }
+      return;
+    }
+    location.href = "/";
+  } catch (e) {
+    alert("Action failed.")
   }
-  location.href = "/";
 }, true);
